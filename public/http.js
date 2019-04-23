7604ca3e7678310234d4d26fcae7944be05cafbc
Paul <paul.r.conte@gmail.com>
2019-04-22 21:16:32 -0700

    add knockout library; refactor js

---
+++ public/http.js
@@ -0,0 +1,33 @@
+function httpGet() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open('GET', '/api/searches');
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            displaySearches(JSON.parse(this.responseText));
+        }
+    };
+    xmlhttp.send();
+}
+
+function httpPost(event) {
+    var xmlhttp = new XMLHttpRequest();
+    var formData = new FormData(document.getElementById('searchForm'));
+
+    event.preventDefault();
+
+    xmlhttp.open('POST', '/api/search', true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            displaySearches(JSON.parse(this.responseText));
+        }
+    };
+    xmlhttp.send(JSON.stringify({
+        'searchString': formData.get('searchString')
+    }));
+}
+
+function displaySearches(searches) {
+    document.getElementById('searchesList').innerHTML = searches;
+}
